57ae065f15ca4b2d85ac01794184fdaa66f821e8
B1lkao <hrabousolesia@gmail.com>
2023-03-11 17:24:03 +0200

    Доробила завдання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+import { GlobalStyle } from './GlobalStyled';
+import { SharedLayout } from './Sharedlayout.jsx/SharedLayout';
+import Home from '../Pages/Home/Home';
+
+const Movies = lazy(() => import('Pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('Pages/MovieDetails/MovieDetails'));
+const Reviews = lazy(() => import('./Rewiews/Reviews'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
+      </Routes>
+      <GlobalStyle />
+    </>
   );
 };
